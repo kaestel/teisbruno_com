e3cd091d775a3da7746b4b706fb0670ca8c1c9b3
kaestel <martin@think.dk>
2015-09-11 11:11:10 +0200

    fixed login problem

--- src/www/login.php
+++ src/www/login.php
@@ -8,14 +8,28 @@ include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
 
 
 $action = $page->actions();
+$model = new User();
 
 
 $page->bodyClass("login");
 $page->pageTitle("Login");
 
 
+if(is_array($action) && count($action)) {
+
+	if(count($action) == 1 && $action[0] == "forgot_password") {
+
+		$page->page(array(
+			"templates" => "pages/forgot_password.php"
+		));
+		exit();
+	}
+
+}
+
+// plain login
 $page->page(array(
-	"type" => "login",
+	"type" => "janitor",
 	"templates" => "pages/login.php"
 ));
 
